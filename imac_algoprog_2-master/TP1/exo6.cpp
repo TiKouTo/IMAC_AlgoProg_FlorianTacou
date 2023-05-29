52388fda8b7a8416b09f8ac43c3d2609e6e3b05e
TiKouTo <115529583+TiKouTo@users.noreply.github.com>
2023-05-29 08:50:29 +0200

    Update exo6.cpp

--- imac_algoprog_2-master/TP1/exo6.cpp
+++ imac_algoprog_2-master/TP1/exo6.cpp
@@ -14,51 +14,78 @@ struct Liste{
 
 struct DynaTableau{
     int* donnees;
+    int taillemax;
+    int taillereelle;
     // your code
 };
 
-
+//pas demandées
 void initialise(Liste* liste)
 {
-
+    Noeud* premier=nullptr;
+    //liste vide 0 = nullptr
 }
 
 bool est_vide(const Liste* liste)
 {
     return false;
 }
-
-void ajoute(Liste* liste, int valeur)
-{
-
-}
-
 void affiche(const Liste* liste)
 {
 
 }
+//demandées
 
-int recupere(const Liste* liste, int n)
+
+void ajoute(Liste* liste, int valeur)
 {
-    return 0;
+    Noeud* nouveaunoeud= new Noeud;
+    
+    Noeud*versledernier=liste->premier->suivant;
+    while (versledernier->suivant!=nullptr)
+    {
+        versledernier=versledernier->suivant;
+    }
+    versledernier->suivant=nouveaunoeud;
+    nouveaunoeud->suivant=nullptr;
+    nouveaunoeud->donnee=valeur;
+}
+
+
+
+Noeud* recupere(const Liste* liste, int n)
+{
+    int compteur=0;
+    Noeud* verslenieme=liste->premier;
+    while(compteur<n)
+    {
+        verslenieme=verslenieme->suivant;
+    }
+    return verslenieme;
 }
 
 int cherche(const Liste* liste, int valeur)
 {
+    Noeud*versceluiqueloncherche=liste->premier;
+    int compteur=0;
+    while(versceluiqueloncherche!=nullptr)
+    {
+        
+        if(versceluiqueloncherche->donnee==valeur)return compteur;
+        versceluiqueloncherche=versceluiqueloncherche->suivant;
+        compteur++;
+    }
     return -1;
 }
 
 void stocke(Liste* liste, int n, int valeur)
 {
-
-}
-
-void ajoute(DynaTableau* tableau, int valeur)
-{
-
+    recupere(liste,n)->donnee=valeur;
 }
 
 
+
+//pas demandées
 void initialise(DynaTableau* tableau, int capacite)
 {
 
@@ -73,44 +100,70 @@ void affiche(const DynaTableau* tableau)
 {
 
 }
-
-int recupere(const DynaTableau* tableau, int n)
+//demandées
+void ajoutetab(DynaTableau* tableau, int valeur)
 {
-    return 0;
+    if(tableau->taillereelle==tableau->taillemax)tableau->taillemax+=100;
+    tableau->donnees[tableau->taillereelle]=valeur;
+    tableau->taillereelle+=1;
+}
+int recuperetab(const DynaTableau* tableau, int n)
+{
+    return tableau->donnees[n];
 }
 
-int cherche(const DynaTableau* tableau, int valeur)
+int cherchetab(const DynaTableau* tableau, int valeur)
 {
+    for (int i=0;i<tableau->taillereelle;i++)
+    {
+        if (tableau->donnees[i])return i;
+    }
     return -1;
 }
 
-void stocke(DynaTableau* tableau, int n, int valeur)
+void stocketab(DynaTableau* tableau, int n, int valeur)
 {
-
+    tableau->donnees[n]=valeur;
 }
 
 //void pousse_file(DynaTableau* liste, int valeur)
 void pousse_file(Liste* liste, int valeur)
 {
-
+    Noeud*nouveaunoeudn2=new Noeud;
+    nouveaunoeudn2->donnee=liste->premier->donnee;
+    nouveaunoeudn2->suivant=liste->premier->suivant;
+    liste->premier->donnee=valeur;
+    liste->premier->suivant=nouveaunoeudn2;    
 }
 
 //int retire_file(Liste* liste)
 int retire_file(Liste* liste)
 {
-    return 0;
+    liste->premier;
+    
+    
+    Noeud*dernier=liste->premier;
+    Noeud*avantdernier=liste->premier;
+    while(dernier->suivant!=nullptr)
+    {
+        avantdernier=dernier;
+        dernier=dernier->suivant;
+    }
+    avantdernier->suivant=nullptr;
+    return dernier->donnee;
+
 }
 
 //void pousse_pile(DynaTableau* liste, int valeur)
 void pousse_pile(Liste* liste, int valeur)
 {
-
+    ajoute(liste,valeur);
 }
 
 //int retire_pile(DynaTableau* liste)
 int retire_pile(Liste* liste)
 {
-    return 0;
+   retire_file(liste); //pour file comme pile on retire le dernier;
 }
 
 
@@ -133,7 +186,7 @@ int main()
 
     for (int i=1; i<=7; i++) {
         ajoute(&liste, i*7);
-        ajoute(&tableau, i*5);
+        ajoutetab(&tableau, i*5);
     }
 
     if (est_vide(&liste))
@@ -152,13 +205,13 @@ int main()
     std::cout << std::endl;
 
     std::cout << "5e valeur de la liste " << recupere(&liste, 4) << std::endl;
-    std::cout << "5e valeur du tableau " << recupere(&tableau, 4) << std::endl;
+    std::cout << "5e valeur du tableau " << recuperetab(&tableau, 4) << std::endl;
 
     std::cout << "21 se trouve dans la liste à " << cherche(&liste, 21) << std::endl;
-    std::cout << "15 se trouve dans la liste à " << cherche(&tableau, 15) << std::endl;
+    std::cout << "15 se trouve dans la liste à " << cherchetab(&tableau, 15) << std::endl;
 
     stocke(&liste, 4, 7);
-    stocke(&tableau, 4, 7);
+    stocketab(&tableau, 4, 7);
 
     std::cout << "Elements après stockage de 7:" << std::endl;
     affiche(&liste);
